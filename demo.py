4f95b8aefde46cf3ae81633092e8a294b0e10433
breezedeus <breezedeus@163.com>
2018-12-17 16:12:52 +0800

    可以用这个脚本跑整个流程

--- demo.py
+++ demo.py
@@ -1,25 +1,48 @@
 # coding:utf-8
+import os
 import time
 from glob import glob
 
 import numpy as np
+import cv2
 from PIL import Image
 
 import model
 # ces
 
-paths = glob('./test/*.*')
 
-if __name__ == '__main__':
-    im = Image.open("./test/test.png")
+def parse_image(img_file):
+    # img_file = "./test/test.png"
+    # img_file = '/Users/king/Documents/WhatIHaveDone/Test/text-detection-ctpn/data/demo/581543991702_.pic_hd.jpg'
+    im = Image.open(img_file)
     img = np.array(im.convert('RGB'))
     t = time.time()
     '''
     result,img,angel分别对应-识别结果，图像的数组，文字旋转角度
     '''
-    result, img, angle = model.model(
+    result, boxed_img, angle = model.model(
         img, model='keras', adjust=True, detectAngle=True)
     print("It takes time:{}s".format(time.time() - t))
-    print("---------------------------------------")
-    for key in result:
-        print(result[key][1])
+    return result, boxed_img
+    # print("---------------------------------------")
+    # for key in result:
+    #     print(result[key][1])
+
+
+if __name__ == '__main__':
+    data_dir = '/Users/king/Documents/WhatIHaveDone/Test/text-detection-ctpn/data/demo'
+    # data_dir = '/Users/king/Documents/Ein/产品/大童/10个保险公司的识别结果/CID0000000334_华夏人寿'
+    paths = glob(os.path.join(data_dir, '*.*'))
+    for fp in paths:
+        if os.path.basename(fp).split('.')[-1] not in {'jpg', 'jpeg', 'png'}:
+            continue
+        print('parsing file %s ...' % fp)
+        result, boxed_im = parse_image(fp)
+        res_fp = '.'.join(os.path.basename(fp).split('.')[:-1]) + '_res.txt'
+        res_fp = os.path.join(data_dir, res_fp)
+        with open(res_fp, 'w') as out_f:
+            out_f.writelines(str(result))
+
+        boxed_im_fp = '.'.join(os.path.basename(fp).split('.')[:-1]) + '_boxed.png'
+        boxed_im_fp = os.path.join(data_dir, boxed_im_fp)
+        cv2.imwrite(boxed_im_fp, boxed_im)
